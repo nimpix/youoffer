f081d1217a958843b09635a313aeba6fbea482ee
nimpix <web@nimpix.ru>
2018-11-23 16:40:59 +0300

    memory leaks

--- frontend/app/src/components/Catalog.vue
+++ frontend/app/src/components/Catalog.vue
@@ -1,10 +1,10 @@
 <template id="catalog-template">
     <div class="user-body">
-        <div v-if="page>1" class="clearfix w-100 mb-4 text-right font-weight-bold title-pagination">Страница {{ page }}</div>
+       <div v-if="page>1" class="clearfix w-100 mb-4 text-right font-weight-bold title-pagination">Страница {{ page }}</div>
         <div class="clearfix btn-group justify-content-end w-100 mb-4">
             <button v-if="page>1" type="button" class="btn btn-sm btn-primary outline" @click="page = 1">В начало</button>
             <button type="button" class="btn btn-sm btn-primary outline" v-if="page != 1" @click="page--"> Предыдущая </button>
-            <button type="button" :class="'btn btn-sm btn-primary outline'+ current " :data="(page == pageNumber) ? '1': '2'" v-for="pageNumber in pages.slice(page-1, page+3)" @click="changePage(pageNumber)"> {{pageNumber}} </button>
+            <button type="button" :class="'btn btn-sm btn-primary outline'+ current " :data="(page == pageNumber) ? '1': '2'" v-for="(pageNumber,index) in pages.slice(page-1, page+3)" :key="index" @click="changePage(pageNumber)"> {{pageNumber}} </button>
             <button type="button" @click="page++" v-if="page < pages.length-4" class="btn btn-sm btn-primary outline"> Следующая </button>
         </div>
         <div class="row">
@@ -27,17 +27,17 @@
                 pages: [],
                 page: 1,
                 perPage:20,
-                current: ''
+                current: '',
             }
         },
         props: {},
         computed: {
             prods() {
-                this.$store.dispatch('set_products');  //вызываем нужный экшон который вызывает свою мутацию
-                return this.$store.getters.get_products  //получаем результат
+                //вызываем нужный экшон который вызывает свою мутацию
+              return this.$store.getters.get_products  //получаем результат
             },
             displayedProds () {
-                return this.paginate(this.prods);
+               return this.paginate(this.prods);
             }
         },
         methods:{
@@ -65,6 +65,9 @@
             prods () {
                 this.setPages();
             }
+        },
+        created:function(){
+            this.$store.dispatch('set_products');
         }
     }
 </script>
--- frontend/app/src/store.js
+++ frontend/app/src/store.js
@@ -15,7 +15,6 @@ export default new Vuex.Store({
   actions: {
     set_products: ({commit}) => {
         fetch('http://offer/backend/web/api-getprods', {
-            // mode: 'no-cors',
             headers: {
                 'Access-Control-Allow-Origin': "*"
             },
